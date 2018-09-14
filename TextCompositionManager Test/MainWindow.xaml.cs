90b1995f2c318336ce28535799d86b6d99126b8f
Tennyleaz <tennyleaz@gmail.com>
2018-09-14 15:52:33 +0800

    Print some event to screen.

---
+++ TextCompositionManager Test/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TextCompositionManager_Test
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            TextCompositionManager.AddPreviewTextInputHandler(tbSearch, OnPreviewTextInput);
+            TextCompositionManager.AddPreviewTextInputStartHandler(tbSearch, OnPreviewTextInputStart);
+            TextCompositionManager.AddPreviewTextInputUpdateHandler(tbSearch, OnPreviewTextInputUpdate);
+        }
+
+        private void OnPreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            tbLog.Text += "PreviewTextInput\n";
+        }
+        private void OnPreviewTextInputStart(object sender, TextCompositionEventArgs e)
+        {
+            tbLog.Text += "PreviewTextInputStart\n";
+        }
+        private void OnPreviewTextInputUpdate(object sender, TextCompositionEventArgs e)
+        {
+            tbLog.Text += "PreviewTextInputStart\n";
+        }
+
+        private void tbSearch_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            tbLog.Text += "\nPreviewKeyDown\n";
+        }
+
+        private void tbSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            tbLog.Text += "TextChanged\n";
+        }
+    }
+}
